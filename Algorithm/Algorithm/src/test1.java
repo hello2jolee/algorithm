c2c3d17be3b731fbbd2cc0bec9ed6b83eebb3836
jioun.lee.dev <jioun.lee.dev@gmail.com>
2019-01-23 23:06:55 +0900

    공백제거

--- Algorithm/Algorithm/src/test1.java
+++ Algorithm/Algorithm/src/test1.java
@@ -5,16 +5,12 @@ public class test1 {
 	class Solution {
 	    public int[] solution(int[] array, int[][] commands) {
 	        int[] answer = {};
-	 
 	        for(int i = 0; i<commands.length; i++){
 	            int[] temp = {};
-	            
 	            for(int j = 0; j<commands[i][1]-commands[i][0]+1; j++)
 	                temp[j] = array[commands[i][0]+j];
-	            
 	            Arrays.sort(temp);
 	            answer[i] = temp[commands[i][2]];
-	 
 	        }    
 	        return answer;
 	    }
